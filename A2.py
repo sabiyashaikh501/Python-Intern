d95faa83b42a6f56500beb2278ba7feb9a4012ce
sabiyashaikh501 <sabiyashaikh501@gmail..com>
2021-01-06 02:00:39 +0530

    first commit

---
+++ A2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  5 16:48:24 2021
+
+@author: Sabiya Shaikh
+"""
+
+
+from datetime import date
+ 
+data_0 = date(2020, 7, 2)
+data_1 = date(2020, 7, 11)
+difference= data_1 - data_0
+ 
+print(difference,"days")
